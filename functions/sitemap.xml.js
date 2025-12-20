91a54118cab7e5cd94d0eabd592ca5beb0b05d21
Patone2B <antone.lomellini@hotmail.com>
2025-12-20 18:34:46 +0100

    Add sitemap XML generation function

---
+++ functions/sitemap.xml.js
@@ -0,0 +1,18 @@
+export async function onRequest() {
+  return new Response(
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://pas-peur-du-pro.pages.dev/</loc>
+    <lastmod>2025-12-10T20:00:00+00:00</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`,
+    {
+      headers: {
+        "Content-Type": "application/xml; charset=UTF-8"
+      }
+    }
+  );
+}
