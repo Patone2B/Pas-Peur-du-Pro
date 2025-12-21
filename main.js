f5c55e088430c016a2f3f175bcaabaee42189d1e
Patone2B <antone.lomellini@hotmail.com>
2025-12-21 18:26:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -607,7 +607,7 @@ document.addEventListener('DOMContentLoaded', function() {
     "   Le premier site web est : http://info.cern.ch",
     " Les créateurs ont vendu GitHub à Microsoft pour… 7,5 milliards $",
     "Le mot “Dream Weaver” (tisseur de rêves) apparaît dans une chanson des années 70 ; les fondateurs l’ont repris pour évoquer la “création de sites rêvés”",
-    "😂 Le logiciel a été lancé initialement sous le nom de ReelTime chez SuperMac Technology, pour la carte de capture Video Spigot, avant d’être acquis par Adobe Systems et rebaptisé « Premiere »";
+    "😂 Le logiciel a été lancé initialement sous le nom de ReelTime chez SuperMac Technology, pour la carte de capture Video Spigot, avant d’être acquis par Adobe Systems et rebaptisé « Premiere »",
   ];
 
   // Mélange aléatoire à chaque chargement
